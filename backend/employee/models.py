fec6d446e40aebcf40906573b7f320575772b687
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-11-14 11:54:16 +0500

    department model, skill model, employee model

--- backend/employee/models.py
+++ backend/employee/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# Department Model
+class DepartmentModel(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+# Skill Model
+class SkillModel(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+# Employee Model
+class EmployeeModel(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    salary = models.DecimalField(decimal_places=2, max_digits=10)
+    hire_date = models.DateField()
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey(DepartmentModel, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(SkillModel)
+    manager = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subordinates')
+    
+    def __str__(self):
+        return self.name
