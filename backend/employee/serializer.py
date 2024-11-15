5d01bd150cc0c0ea97e3e78e01f5d4a80398f5e1
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-11-15 11:09:12 +0500

    department serializer, skill serializer, employee serializer

---
+++ backend/employee/serializer.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import DepartmentModel, SkillModel, EmployeeModel
+
+# Department Serializer
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DepartmentModel
+        fields = ['id', 'name']
+        
+# Skill Serializer
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillModel
+        fields = ['id', 'name']
+       
+# Employee Serializer 
+class EmployeeSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    position = serializers.CharField(max_length=100)
+    salary = serializers.DecimalField(decimal_places=2, max_digits=10)
+    hire_date = serializers.DateField()
+    email = serializers.EmailField(validators=[serializers.UniqueValidator(queryset=EmployeeModel.objects.all())])
+    department = serializers.PrimaryKeyRelatedField(queryset=DepartmentModel.objects.all())
+    skills = serializers.PrimaryKeyRelatedField(many=True, queryset=SkillModel.objects.all())
+    manager = serializers.PrimaryKeyRelatedField(queryset=EmployeeModel.objects.all(), required=False, allow_null=True)
+    
+    def create(self, validated_data):
+        skills = validated_data.pop('skills')
+        employee = EmployeeModel.objects.create(**validated_data)
+        employee.skills.set(skills)
+        
+        return employee
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.position = validated_data.get('position', instance.position)
+        instance.salary = validated_data.get('salary', instance.salary)
+        instance.hire_date = validated_data.get('hire_date', instance.hire_date)
+        instance.email = validated_data.get('email', instance.email)
+        instance.department = validated_data.get('department', instance.department)
+        instance.manager = validated_data('manager', instance.manager)
+        
+        if 'skills' in validated_data:
+            skills = validated_data.pop('skills')
+            instance.skills.set(skills)
+            
+        instance.save()
+        
+        return instance
