beb71db5a5a9f29d12a66c97862a0d8d291d0413
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-11-19 22:26:22 +0500

    backend: skill put view, skill get view, department put view, departmen get view

--- backend/employee/views.py
+++ backend/employee/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .models import DepartmentModel, SkillModel, EmployeeModel
 from .serializer import DepartmentSerializer, SkillSerializer, EmployeeSerializer
 
+# Get Departments, Create Deparment
 class DepartmentListCreate(APIView):
     def get(self, request):
         departments = DepartmentModel.objects.all()
@@ -23,6 +24,7 @@ class DepartmentListCreate(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Get, Put, Delete Department
 class DepartmentDetail(APIView):
     def delete(self, request, pk):
         department = get_object_or_404(DepartmentModel, pk=pk)
@@ -30,8 +32,27 @@ class DepartmentDetail(APIView):
         department.delete()
         
         return Response({'Message': 'Department Deleted'}, status=status.HTTP_200_OK)
-
     
+    def put(self, request, pk):
+        department = get_object_or_404(DepartmentModel, pk=pk)
+        
+        serializer = DepartmentSerializer(department, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, pk):
+        deparment = get_object_or_404(DepartmentModel, pk=pk)
+        
+        serializer = DepartmentSerializer(deparment)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# Get Skills, Create Skill
 class SkillListCreate(APIView):
     def get(self, request):
         skills = SkillModel.objects.all()
@@ -49,7 +70,8 @@ class SkillListCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+# Get, Put, Delete Skill  
 class SkillDetail(APIView):
     def delete(self, request, pk):
         skill = get_object_or_404(SkillModel, pk=pk)
@@ -57,8 +79,25 @@ class SkillDetail(APIView):
         skill.delete()
         
         return Response({'Message': 'Skill Deleted'}, status=status.HTTP_200_OK)
-        
     
+    def get(self, request, pk):
+        skill = get_object_or_404(SkillModel, pk=pk)
+        
+        serializer = SkillSerializer(skill)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        skill = get_object_or_404(SkillModel, pk=pk)
+        
+        serializer = SkillSerializer(skill, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+# Get Employees, Create Employee
 class EmployeeListCreate(APIView):
     def get(self, request):
         employees = EmployeeModel.objects.all()
@@ -77,6 +116,7 @@ class EmployeeListCreate(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+# Get, Put, Delete Employee
 class EmployeeDetail(APIView):
     def get(self, request, pk):
         employee = get_object_or_404(EmployeeModel, pk=pk)
