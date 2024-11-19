7103ef5bfbc43c610c92d75309795dd873e25974
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-11-19 15:59:27 +0500

    backend: cors configurations

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'employee',
 ]
 
@@ -39,7 +40,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
