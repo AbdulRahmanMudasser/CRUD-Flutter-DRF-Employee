bd581e7124a00423c94545c2af952e51847140f2
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-11-19 10:43:06 +0500

    backend: allowed host addresses

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,8 +13,10 @@ SECRET_KEY = 'django-insecure-1xvo2_)y(2#44_ag09q0@43fy*^c=d#7m4utg316ag#z@cc&(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+try:
+    from .config import ALLOWED_HOSTS
+except ImportError:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,7 @@ from employee import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('departments/', views.DepartmentList.as_view(), name='department-list'),
+    path('departments/', views.DepartmentListCreate.as_view(), name='department-list-create'),
     path('skills/', views.SkillList.as_view(), name='skills-list'),
     path('employees/', views.EmployeeListCreate.as_view(), name='employee-list-create'),
     path('employees/<int:pk>', views.EmployeeDetail.as_view(), name='employee-detail'),
